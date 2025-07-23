414a6e3276d572a32a7addf367120edd84935fba
Greg <gregcourt10@gmail.com>
2025-07-23 10:34:39 +0100

    azure bootstrapped

--- infra/azure-bootstrap/main.tf
+++ infra/azure-bootstrap/main.tf
@@ -4,12 +4,12 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_key_vault" "homelab" {
-  name                        = "kv-homelab-uks"
-  location                    = azurerm_resource_group.main.location
-  resource_group_name         = azurerm_resource_group.main.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  enable_rbac_authorization   = true
+  name                      = "kv-homelab-uks"
+  location                  = azurerm_resource_group.main.location
+  resource_group_name       = azurerm_resource_group.main.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  enable_rbac_authorization = true
 }
 
 resource "azurerm_storage_account" "homelab" {
@@ -21,10 +21,37 @@ resource "azurerm_storage_account" "homelab" {
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_id    = azurerm_storage_account.homelab.id
+  name               = "tfstate"
+  storage_account_id = azurerm_storage_account.homelab.id
 }
 
 resource "azuread_application" "gh_homelab" {
   display_name = "gh-homelab"
+}
+
+resource "azuread_application_federated_identity_credential" "gh_homelab_oidc" {
+  application_id = azuread_application.gh_homelab.id
+
+  display_name = "gh-actions-main"
+  description  = "OIDC federated credential for GitHub Actions (main branch)"
+
+  issuer    = "https://token.actions.githubusercontent.com"
+  subject   = "repo:greg-court/homelab:ref:refs/heads/main"
+  audiences = ["api://AzureADTokenExchange"]
+}
+
+resource "azuread_service_principal" "gh_homelab" {
+  client_id = azuread_application.gh_homelab.client_id
+}
+
+resource "azurerm_role_assignment" "kv_secrets_user" {
+  scope                = azurerm_key_vault.homelab.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azuread_service_principal.gh_homelab.object_id
+}
+
+resource "azurerm_role_assignment" "storage_blob_contributor" {
+  scope                = azurerm_storage_account.homelab.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azuread_service_principal.gh_homelab.object_id
 }
