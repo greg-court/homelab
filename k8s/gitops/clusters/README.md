0ca1af9768ea71657db8c19f5fa23c979edd2e75
Greg <gregcourt10@gmail.com>
2025-07-27 12:32:00 +0100

    updated repo structure for gitops

---
+++ k8s/gitops/clusters/README.md
@@ -0,0 +1,2 @@
+root-apps.yaml is the single parent Application Argo CD syncs after Terraform
+installs. A minimal stub keeps bootstrap deterministic.
--- k8s/terraform/cluster-trust/argocd.tf
+++ k8s/terraform/cluster-trust/argocd.tf
@@ -1,4 +1,6 @@
 module "argocd" {
-  source = "../tf-modules/k8-argocd"
-  namespace = "argocd"
+  source               = "../tf-modules/k8-argocd"
+  namespace            = "argocd"
+  bootstrap_repo_url   = "https://github.com/greg-court/homelab.git"
+  bootstrap_repo_path  = "k8s/gitops/clusters/trust"
 }
