353c41bccc7f2d6dda3498ad2a07823ea7633412
Greg <gregcourt10@gmail.com>
2025-08-02 19:15:27 +0100

    updates

--- k8s/terraform/hosts-custom/main.tf
+++ k8s/terraform/hosts-custom/main.tf
@@ -1,5 +1,54 @@
+locals {
+  base_vm = {
+    tags             = ["talos"]
+    on_boot          = true
+    agent            = true
+    bios             = "seabios"
+    cpu              = { cores = 2 }
+    memory           = { dedicated = 4096 }
+    operating_system = { type = "l26" }
+    disks            = { "scsi0" = { size = 16 } }
+    cdrom            = { file_id = "nfs-hdd:iso/talos-nocloud-amd64-qemu.iso" }
+  }
+}
+
+locals {
+  vms = merge([
+    for cluster_name, c in var.clusters : {
+      for host_id, h in c.hosts :
+      upper(host_id) => merge(
+        local.base_vm,
+        {
+          tags = distinct(concat(local.base_vm.tags, lookup(c, "extra_tags", [])))
+
+          network_devices = [{
+            vlan_id     = c.vlan_id
+            mac_address = h.mac_address
+          }]
+
+          node_name = h.node_name
+
+          talos = {
+            cluster = cluster_name
+            type    = "controlplane"
+            patch_data = yamlencode({
+              machine = {
+                network    = { hostname = "${lower(host_id)}.internal" }
+                nodeLabels = { zone = c.zone }
+              }
+              cluster = {
+                allowSchedulingOnControlPlanes = true
+              }
+            })
+          }
+        }
+      )
+    }
+  ]...)
+}
+
 module "proxmox_vms" {
   source              = "../../../tf-modules/proxmox-vms"
-  vms                 = var.hosts
+  vms                 = local.vms               # <-- flattened map
   talos_templates_dir = "${path.module}/talos_config"
 }
--- k8s/terraform/hosts-custom/variables.tf
+++ k8s/terraform/hosts-custom/variables.tf
@@ -1,2 +1,14 @@
 variable "endpoint" { default = "https://pve02.internal:8006/api2/json" }
-variable "hosts" {}
+variable "clusters" {
+  description = "Per-cluster defaults and per-host deltas"
+  type = map(object({
+    vlan_id    = number
+    zone       = string
+    extra_tags = optional(list(string), [])
+
+    hosts = map(object({
+      mac_address = string
+      node_name   = string
+    }))
+  }))
+}
