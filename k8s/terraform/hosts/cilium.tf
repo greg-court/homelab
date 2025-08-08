aea6886cff217aca0d02afe54ce67950d0ba4102
Greg <gregcourt10@gmail.com>
2025-08-08 13:35:06 +0100

    increased to 45s

--- k8s/terraform/hosts/cilium.tf
+++ k8s/terraform/hosts/cilium.tf
@@ -37,13 +37,13 @@ locals {
 
 # Wait a bit after the API is reachable for TRUST
 resource "time_sleep" "after_bootstrap_trust" {
-  create_duration = "30s"
+  create_duration = "45s"
   depends_on      = [talos_cluster_kubeconfig.kc["cluster-trust"]]
 }
 
 # Wait a bit after the API is reachable for DMZ
 resource "time_sleep" "after_bootstrap_dmz" {
-  create_duration = "30s"
+  create_duration = "45s"
   depends_on      = [talos_cluster_kubeconfig.kc["cluster-dmz"]]
 }
 
