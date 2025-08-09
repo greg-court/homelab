d23ef6c5090291c878fc8bfe3b128612c80c8902
Greg <gregcourt10@gmail.com>
2025-08-09 19:31:59 +0100

    building terraform v2

---
+++ k8s/terraformv2/clusters/cluster-dmz/main.tf
@@ -0,0 +1,17 @@
+module "argocd" {
+  source = "../modules/argocd"
+
+  namespace           = "argocd"
+  bootstrap_repo_url  = "https://github.com/greg-court/homelab.git"
+  bootstrap_repo_path = "k8s/gitops/clusters/dmz"
+}
+
+provider "kubernetes" {
+  config_path = "../hosts/configs/cluster-dmz/kubeconfig"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "../hosts/configs/cluster-dmz/kubeconfig"
+  }
+}
