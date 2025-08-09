2527f99a55439b8d18fb3ee197308de7d5261a45
Greg <gregcourt10@gmail.com>
2025-08-09 19:39:24 +0100

    bugfix

--- k8s/terraform/variables.tf
+++ k8s/terraform/variables.tf
@@ -1,4 +1,6 @@
 variable "endpoint" { default = "https://pve02.internal:8006/api2/json" }
 variable "clusters" { type = any }
-
 variable "install_image" { default = "factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.10.5" }
+variable "azure_tenant_id" { sensitive = true }
+variable "azure_client_id" { sensitive = true }
+variable "azure_client_secret" { sensitive = true }
