7a99398c889b3b4749bffe6fb4e0405d8aabe183
Greg <gregcourt10@gmail.com>
2025-08-09 20:11:48 +0100

    bugfix

--- k8s/bootstrap/modules/argocd/main.tf
+++ k8s/bootstrap/modules/argocd/main.tf
@@ -25,7 +25,7 @@ locals {
       clusters = {
         inCluster = {
           enabled = true
-          # When enabled the chart:
+          # When incluster is enabled the chart:
           #   - creates a ServiceAccount called `argocd-manager`
           #   - binds it (cluster-admin by default) so it can talk to the API
           #   - writes a kubeconfig for https://kubernetes.default.svc
@@ -41,6 +41,8 @@ locals {
   })
 }
 
+
+# 1) Install Argo CD (installs CRDs)
 resource "helm_release" "argocd" {
   name       = "argocd"
   namespace  = kubernetes_namespace.argocd.metadata[0].name
@@ -50,35 +52,40 @@ resource "helm_release" "argocd" {
 
   create_namespace = false
   values           = [local.base_values]
+  wait             = true
 }
 
-# ---------- bootstrap “app-of-apps” -----------------------------------------
-resource "kubernetes_manifest" "root_app" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
-    metadata = {
-      name       = "root-apps"
-      namespace  = kubernetes_namespace.argocd.metadata[0].name
-      finalizers = ["resources-finalizer.argocd.argoproj.io"]
-    }
-    spec = {
-      project = "default"
-      destination = {
-        server    = "https://kubernetes.default.svc"
+# 2) Bootstrap the root app using argocd-apps chart (avoids CRD race)
+resource "helm_release" "argocd_root_apps" {
+  name       = "argocd-root-apps"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-apps"
+  # (optionally pin a version later)
+
+  values = [yamlencode({
+    applications = [
+      {
+        name      = "root-apps"
         namespace = kubernetes_namespace.argocd.metadata[0].name
+        project   = "default"
+        source = {
+          repoURL        = var.bootstrap_repo_url
+          targetRevision = var.bootstrap_repo_revision
+          path           = var.bootstrap_repo_path
+          directory      = { recurse = true }
+        }
+        destination = {
+          server    = "https://kubernetes.default.svc"
+          namespace = kubernetes_namespace.argocd.metadata[0].name
+        }
+        syncPolicy = {
+          automated   = { prune = true, selfHeal = true }
+          syncOptions = ["CreateNamespace=true"]
+        }
       }
-      source = {
-        repoURL        = var.bootstrap_repo_url
-        targetRevision = var.bootstrap_repo_revision
-        path           = var.bootstrap_repo_path
-        directory      = { recurse = true }
-      }
-      syncPolicy = {
-        automated   = { prune = true, selfHeal = true }
-        syncOptions = ["CreateNamespace=true"]
-      }
-    }
-  }
+    ]
+  })]
+
   depends_on = [helm_release.argocd]
 }
