1dd2e7a5f0beea06958109d8a78e806a00e552df
Greg <gregcourt10@gmail.com>
2025-08-12 19:32:33 +0100

    bugfixes

--- k8s/bootstrap/00-talos-config/variables.tf
+++ k8s/bootstrap/00-talos-config/variables.tf
@@ -1,6 +1,7 @@
 variable "cluster_name" { type = string }     # "homelab"
 variable "install_disk" { type = string }     # "/dev/sda"
 variable "cluster_endpoint" { type = string } # "https://k8s-01:6443"
+variable "api_server" { type = string }       # "api.klab.internal"
 variable "bootstrap_node" { type = string }   # "k8s-01"
 variable "storage_account_name" { type = string }
 variable "container_name" { type = string }
