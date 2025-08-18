4917b8a6bee07110c6b7f35b828afff0af4c61fe
Greg <gregcourt10@gmail.com>
2025-08-18 09:55:27 +0100

    readded interfaces

--- k8s/talos/main.tf
+++ k8s/talos/main.tf
@@ -31,8 +31,8 @@ locals {
           vlans = [
             { vlanId = 3, dhcp = true },
             { vlanId = 4, dhcp = true },
-            # { vlanId = 5, dhcp = true },
-            # { vlanId = 6, dhcp = true }
+            { vlanId = 5, dhcp = true },
+            { vlanId = 6, dhcp = true }
           ]
         }]
       }
